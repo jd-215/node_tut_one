0f949e5174c67ce8237b4d2efc4837fde48bba23
jash215 <58883072+jd-215@users.noreply.github.com>
2024-01-13 22:12:23 +0530

    changes done

--- server/index.js
+++ server/index.js
@@ -38,8 +38,8 @@ dotenv.config({ path: "./.env" })
 connectDB()
     .then(() => {
     app.listen(process.env.PORT || 8000, () => {
-        // console.log(`example app listening on port ${process.env.PORT}`);
-        console.log(`App started`);
+        console.log(`Server listening on port ${process.env.PORT}`);
+        // console.log(`App started`);
         })
     })
     .catch((err) => {
