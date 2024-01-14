2c92d90229a2cd31bd89fb2b2d9a509e1ffcf1bc
jash215 <58883072+jd-215@users.noreply.github.com>
2024-01-14 10:43:35 +0530

    changes added

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,16 +1,12 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/api.ts
-
-import express, { Router } from "express";
+import dotenv from "dotenv";
 import serverless from "serverless-http";
 // import masterApp from "../../server";
 import app from "../../server";
 
-const router = Router();
 dotenv.config({ path: "./.env" });
 // router.get("/hello", (req, res) => res.send("Hello World!"));
 
-api.use("/api/", router);
-
 // export const handler = serverless(api);
 // connectDB()
-export const handler = serverless(masterApp);
+export const handler = serverless(app);
